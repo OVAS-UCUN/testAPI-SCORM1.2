918465b3e7a71269734d18ef80daefd0815d935d
marlacp <macapripe@gmail.com>
2022-02-15 11:44:27 -0500

    se agrega boton siguiente y atras, también se guarda el nombre en localstorage

--- quizlibJS/js/getDataSCORM.js
+++ quizlibJS/js/getDataSCORM.js
@@ -1,13 +1,30 @@
 window.addEventListener("load", function(event) {
-    console.log("Todos los recursos del SCORM terminaron de cargar!");
+    // console.log("Todos los recursos del SCORM terminaron de cargar!");
     // Busca el campo para llenar con el nombre del usuario
     let userFieldSCORM = document.getElementById('userName');
     if (userFieldSCORM) {
         let getUsername = String(scorm.get("cmi.core.student_name"));
-        console.log(getUsername);
-        if (getUsername !== "null") {
-            console.log("La API de SCORM conecta con el nombre del usuario");
-            userFieldSCORM.innerHTML = getUsername;
+        // console.log(getUsername);
+
+        function savelocal() {
+            if (getUsername !== "null") {
+                localStorage.setItem("nombre", getUsername);
+
+            }
         }
+        savelocal();
+
+        function obtenerlocal() {
+            let nombre = localStorage.getItem("nombre");
+            // console.log("nombre", nombre);
+
+            if (nombre !== "null") {
+                console.log("La API de SCORM conecta con el nombre del usuario");
+                userFieldSCORM.innerHTML = nombre;
+            }
+        }
+        obtenerlocal();
+
+
     }
 });
