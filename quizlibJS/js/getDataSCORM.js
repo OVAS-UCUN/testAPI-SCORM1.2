ee4376fcc645362f4a49474818302e899ff73b34
Diego Javier Mena Amado <ingelectronicadj@gmail.com>
2022-02-16 21:22:37 -0500

    test de cierre de API correcto, evita almacenamiento de datos en LocalStorage

--- quizlibJS/js/getDataSCORM.js
+++ quizlibJS/js/getDataSCORM.js
@@ -1,30 +1,17 @@
 window.addEventListener("load", function(event) {
-    // console.log("Todos los recursos del SCORM terminaron de cargar!");
+    console.log("Todos los recursos del SCORM terminaron de cargar!");
     // Busca el campo para llenar con el nombre del usuario
     let userFieldSCORM = document.getElementById('userName');
     if (userFieldSCORM) {
         let getUsername = String(scorm.get("cmi.core.student_name"));
-        // console.log(getUsername);
-
-        function savelocal() {
-            if (getUsername !== "null") {
-                localStorage.setItem("nombre", getUsername);
-
-            }
+        console.log(getUsername);
+        if (getUsername !== "null") {
+            console.log("La API de SCORM conecta con el nombre del usuario");
+            localStorage.setItem("USER_LMS", getUsername);
+        } else {
+            console.log("el nombre del usuario lo trae del LocalStorage");
+            getUsername = localStorage.getItem("USER_LMS");
         }
-        savelocal();
-
-        function obtenerlocal() {
-            let nombre = localStorage.getItem("nombre");
-            // console.log("nombre", nombre);
-
-            if (nombre !== "null") {
-                console.log("La API de SCORM conecta con el nombre del usuario");
-                userFieldSCORM.innerHTML = nombre;
-            }
-        }
-        obtenerlocal();
-
-
+        userFieldSCORM.innerHTML = getUsername;
     }
 });
